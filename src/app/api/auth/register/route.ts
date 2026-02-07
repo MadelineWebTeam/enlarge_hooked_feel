6b679ea1e5d832b0904095470cfe368e6684a6c9
Eynar <yordanopv@gmail.com>
2026-02-06 19:31:25 -0600

    add validation duplicated user

--- src/app/api/auth/register/route.ts
+++ src/app/api/auth/register/route.ts
@@ -1,13 +1,31 @@
 import { prisma } from "@/lib/prisma"
-import bcrypt from "bcrypt"
+import bcrypt from "bcryptjs"
 import { NextResponse } from "next/server"
 
 export async function POST(req: Request) {
   const { email, password, name } = await req.json()
 
+  if (!email || !password) {
+    return NextResponse.json(
+      { error: "Missing fields" },
+      { status: 400 }
+    )
+  }
+
+  const existing = await prisma.user.findUnique({
+    where: { email },
+  })
+
+  if (existing) {
+    return NextResponse.json(
+      { error: "Email already exists" },
+      { status: 409 }
+    )
+  }
+
   const hashed = await bcrypt.hash(password, 10)
 
-  const user = await prisma.user.create({
+  await prisma.user.create({
     data: {
       email,
       name,
